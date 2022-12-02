80e6baa4027d913afefe94e9bca2ad0d7f6bf184
Fyl Andrii <fil.andrey91@gmail.com>
2022-12-02 16:52:09 +0200

    adds other code variant in reducer.jsx

--- src/components/StatusFilter/StatusFilter.jsx
+++ src/components/StatusFilter/StatusFilter.jsx
@@ -6,7 +6,10 @@ import { setStatusFilter } from '../../redux/actions';
 import css from './StatusFilter.module.css';
 
 export const StatusFilter = () => {
+  // в змінну dispatch записуємо хук useDispatch(), який транспортує небхідний action
+  // до store
   const dispatch = useDispatch();
+  // в змінну filter записуємо хук useSelector, в який передаємо значення зі state
   const filter = useSelector(getStatusFilter);
 
   const handleFilterChange = filter => dispatch(setStatusFilter(filter));
--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,8 +1,12 @@
 import { createStore } from 'redux';
 // зв'язуємо логіку Redux DevTools з інструментами розробника в браузері
 import { devToolsEnhancer } from '@redux-devtools/extension';
-
+//  іьпортуємо кореневий редюсер (тільки 1 на весь застосунок), який саме і опрацьовує
+// всі відправленні actions і вироховує новій state
 import { rootReducer } from './reducer';
-
+// в змінну enhancer передаємо хук devToolsEnhancer(), який зв'яже інструменти з браузером
 const enhancer = devToolsEnhancer();
+// в стор передаємо кореневий редюсер, що виконуватиме всі обчислення та
+// enhancer - для підключення інструментів розробника з браузером
+
 export const store = createStore(rootReducer, enhancer);
